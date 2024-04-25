7222e4c4aff449f7b9c9906a72ac51cd8708cfbe
avanronaufaladi <avaroadi@gmail.com>
2024-04-25 11:24:44 +0700

    menambahkan pencarian data

--- 199_singlelinkedlist/199_singlelinkedlist.cpp
+++ 199_singlelinkedlist/199_singlelinkedlist.cpp
@@ -84,4 +84,12 @@ struct Node {
          return;
      }
  }
- 
+ void searchData() {
+     if (listEmpty()) {
+         cout << "List Kosong" << endl;
+         system("pause");
+         system("cls");
+         return;
+     }
+
+   
