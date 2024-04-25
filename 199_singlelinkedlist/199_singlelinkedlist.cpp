4a97fee888849e566aae7c52e22471f6cf34357e
avanronaufaladi <avaroadi@gmail.com>
2024-04-25 11:19:52 +0700

    perulangan data yang masuk

--- 199_singlelinkedlist/199_singlelinkedlist.cpp
+++ 199_singlelinkedlist/199_singlelinkedlist.cpp
@@ -49,4 +49,8 @@ struct Node {
  bool searchNode(int nim, Node* current, Node* previous) {
      previous = START;
      current = START;
+     while (current != NULL && nim > current->noMhs) {
+         previous = current;
+         current = current->next;
+     }
    
